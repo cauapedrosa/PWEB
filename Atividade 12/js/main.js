858596b5f10119c79a75eb3ecc864b1417a36377
Cauã <cauapedrosa@live.com>
2021-10-29 21:12:12 -0300

    add sfx
    + now actions stop after glass shattering

--- Atividade 12/js/main.js
+++ Atividade 12/js/main.js
@@ -1,10 +1,12 @@
 const log = console.log;
 
-
 function hover() {
     win.setAttribute(`src`, `./img/window-open.png`);
+    document.getElementById('sfx-open').play();
+    document.getElementById('topbar').innerHTML = `<h1>Atividade 12</h1><br>`
 }
 
 function unhover() {
     win.setAttribute(`src`, `./img/window-closed.png`);
+    document.getElementById('sfx-close').play();
 }
